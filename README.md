68770f42bbfef36f777942272871d7be027f0b38
ayoub <ubmagh@gmail.com>
2025-10-19 17:39:59 +0100

    v1 trust me

--- README.md
+++ README.md
@@ -2,6 +2,13 @@
 
 Easily find that instance you wanna ssh into, instead of browsing commands history for so long. 
 
+## How it works 
+
+Construct the default inventory file `~/.config/taq.inventory.yaml` following examples on `example-inventories/`.
+Then execute the binary `taq` to find quickly the target instance by comma/space separated keywords.
+The default inventory file `~/.config/taq.inventory.yaml` can be altered by setting the env variable `TAQ_INVENTORY_PATH`.
+
+
 ## Todos 
 
 - nested groups ? 
--- main.go
+++ main.go
@@ -8,8 +8,41 @@ import (
 	"github.com/ubmagh/taq/search"
 )
 
+func printHelp() {
+	fmt.Println(`
+	taq - fast SSH search and connect CLI
+	
+	Usage:
+	taq            # launch interactive search
+	taq --help,-h  # show this help message
+	taq --version,-v  # show version
+
+	Config:
+	TAQ_INVENTORY_PATH : environment variable to specify inventory file path, default : "~/.config/taq.inventory.yaml"
+
+	Features:
+	• Search hosts by name, address, user, and labels
+	• Interactive fuzzy search with up/down arrows
+	• Launch SSH session directly from the list
+	• Uses inventory from YAML file`)
+}
+
 func main() {
 	const Version = "v1.0.0"
+
+	if len(os.Args) > 1 {
+		arg := os.Args[1]
+
+		switch arg {
+		case "--version", "-v":
+			fmt.Println("taq", Version)
+			return
+		case "--help", "-h":
+			printHelp()
+			return
+		}
+	}
+
 	if len(os.Args) > 1 && (os.Args[1] == "--version" || os.Args[1] == "-v") {
 		fmt.Println("taq", Version)
 		return
